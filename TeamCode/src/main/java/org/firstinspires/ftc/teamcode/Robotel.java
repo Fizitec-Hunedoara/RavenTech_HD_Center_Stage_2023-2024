7b17952be2134230286389918e91d0e772b2c85d
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-09-23 13:00:58 +0300

    <bug fix> deleted front motors (#4)
    Co-authored-by: RobotX Hunedoara <robotxbp@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotel.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class Robotel extends OpMode {
-    public DcMotorEx motorFL,motorFR;
+    public DcMotorEx motorFL,motorFR,motorBR,motorBL;
     @Override
     public void init() {
 
