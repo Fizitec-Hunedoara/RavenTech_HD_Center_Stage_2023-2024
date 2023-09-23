d706750c0e77137aa9362b237ffdb7709608512d
RobotX Hunedoara <robotxbp@gmail.com>
2023-09-23 12:25:41 +0300

    <feature> added chasis motors to program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotel.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 
 @TeleOp
 public class Robotel extends OpMode {
+    public DcMotorEx motorBR,motorBL,motorFL,motorFR;
     @Override
     public void init() {
 
