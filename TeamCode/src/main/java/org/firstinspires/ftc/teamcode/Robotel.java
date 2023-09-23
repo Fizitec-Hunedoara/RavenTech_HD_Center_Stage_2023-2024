0aed3cd5f380136e7d4b48e9c464ed38c8c90e67
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-09-23 12:47:02 +0300

    Test (#2)
    * <feature> added base OpMode
    
    * <feature> added chasis motors to program
    
    ---------
    
    Co-authored-by: RobotX Hunedoara <robotxbp@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotel.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+@TeleOp
+public class Robotel extends OpMode {
+    public DcMotorEx motorBR,motorBL,motorFL,motorFR;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
